d04c91a2620cea144e29464b4ff2fac842849673
JasonLin0704 <71300686+JasonLin0704@users.noreply.github.com>
2023-03-12 19:02:37 +0800

    Update lab1.py

--- 1112_UNIX/lab1/lab1.py
+++ 1112_UNIX/lab1/lab1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pwn import *
+from pwn import * # note: source ~/pwntools/bin/activate
 import pow as pw
 import base64
 import hashlib
@@ -8,32 +8,26 @@ import hashlib
 r = remote("up23.zoolab.org", 10363)
 pw.solve_pow(r)
 
+# how many loops
 tmp = r.recvuntil(b'complete', drop=True).decode()
 tmp = r.recvuntil(b'challenges', drop=True).decode()
 n = int(tmp.split(' ')[2])
-print('n:', n)
 
+# calculate
 for i in range(0, n):
     tmp = r.recvuntil(b':', drop=True).decode()
-    tmp = r.recvuntil(b'?', drop=True).decode()
-    a = int(tmp.split()[0])
-    op = tmp.split()[1]
-    b = int(tmp.split()[2])
-    print(i, a, op, b)
-    ans = 0
-    if(op == '+'):
-        ans = a + b
-    elif(op == '*'):
-        ans = a * b
-    elif(op == '//'):
-        ans = a // b
-    elif(op == '%'):
-        ans = a % b
-    elif(op == '**'):
-        ans = pow(a, b)
+    tmp = r.recvuntil(b'=', drop=True).decode()
+    a, op, b = int(tmp.split(' ')[1]), tmp.split(' ')[2], int(tmp.split(' ')[3])
+    print(i + 1, a, op, b)
+    
+    if(op == '+'): ans = a + b
+    elif(op == '*'): ans = a * b
+    elif(op == '//'): ans = a // b
+    elif(op == '%'): ans = a % b
+    elif(op == '**'): ans = pow(a, b)
 
     res = ans.to_bytes((ans.bit_length() + 7) // 8, byteorder='little')
-    r.sendline(base64.b64encode(res))
+    r.sendlineafter(b'?', base64.b64encode(res))
 
 r.interactive()
 r.close()
