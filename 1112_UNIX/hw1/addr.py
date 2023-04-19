ed9fbbc9c2fa8589dd7a312ca02b1a36196faa4d
JasonLin0704 <71300686+JasonLin0704@users.noreply.github.com>
2023-04-19 14:59:12 +0800

    Delete addr.py

--- 1112_UNIX/hw1/addr.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-elf = ELF('./launcher')
-print("{:<12s} {:<8s} {:<8s}".format("Func", "GOT", "Address"))
-
-for g in sorted(elf.got):
-    print("{:<12s} {:<8x} {:<8x}".format(g, elf.got[g], elf.symbols[g]))
